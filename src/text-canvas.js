5f633b962fc30a150fc397c501b32bba1b23f58b
Luís Rodrigues <netslider25@gmail.com>
2016-11-07 09:41:37 +0000

    Fixed a critical issue when wordWrap is set to false

--- src/text-canvas.js
+++ src/text-canvas.js
@@ -199,10 +199,6 @@ export default class TextCanvas {
     // Create lines out of the hard line breaks
     const forcedLines = this._text.split('\n')
 
-    if (!this._style.wordWrap) {
-      return forcedLines
-    }
-
     const spaceMeasure = this._ctx.measureText(' ')
     const lines = []
     const lineHeight = this._style.lineHeight || this._style.fontSize * 1.2
@@ -222,7 +218,7 @@ export default class TextCanvas {
         wordMeasure = this._ctx.measureText(lineWords[j])
 
         // The word will not fit the current line
-        if (currentLine.width + wordMeasure.width > this._style.wordWrap) {
+        if (this._style.wordWrap && currentLine.width + wordMeasure.width > this._style.wordWrap) {
           // Only add the word to the next line if there is a previous line
           if (j && i) {
             currentLine.text = currentLine.text.trim()
